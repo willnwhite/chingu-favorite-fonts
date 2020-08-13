820e26de6334fcfa3697090cebbe1c8890467eda
Will White <willnwhite@users.noreply.github.com>
2020-08-13 10:37:02 +0100

    user can manually request more fonts

--- src/Main.elm
+++ src/Main.elm
@@ -1,27 +1,39 @@
 import Html exposing (..)
 import Html.Attributes exposing (style, rel, href)
+import Html.Events exposing (onClick)
 import Browser
 import RemoteData exposing (WebData, RemoteData(..))
 import Json.Decode exposing (..)
 import Http exposing (expectJson)
+import List.Extra exposing (groupsOf)
 
 
 
 -- MODEL
 
 type alias Fonts = List Font
+
 type alias Font =
   { family : String
   , category : String
   }
+  
 type alias Model =
   { fonts : WebData Fonts
+  , visibleFonts : Fonts
+  , restOfFonts : Fonts
   , text : String
-  }
+  } -- perhaps refactor so that visible fonts is part of fonts
+
+n = 8 -- number of fonts to get at a time
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-  ( { fonts = Loading, text = "Making the Web Beautiful!" }
+  ( { fonts = Loading
+    , visibleFonts = []
+    , restOfFonts = []
+    , text = "Making the Web Beautiful!"
+    }
   , Http.get
       { url = "https://www.googleapis.com/webfonts/v1/webfonts?sort=popularity&key=AIzaSyDXdgHuIP_D5ySRE5oA-Hd2qoZaaDBPCO4"
       , expect = expectJson (RemoteData.fromResult >> FontsResponse) decodeFonts
@@ -32,12 +44,29 @@ init _ =
 
 -- UPDATE
 
-type Msg = FontsResponse (WebData Fonts)
+type Msg =
+  FontsResponse (WebData Fonts)
+  | MoreFonts
 
 update msg model =
   case msg of
     FontsResponse response ->
-      ( { model | fonts = response }
+      case response of
+        Success fonts ->
+          ( { model | fonts = response
+            , restOfFonts = List.drop n fonts -- members of the list after n
+            , visibleFonts = List.take n fonts -- first n members of the list
+            }
+          , Cmd.none
+          )
+
+        _ ->
+          ( { model | fonts = response }, Cmd.none)
+
+    MoreFonts ->
+      ( { model | visibleFonts = model.visibleFonts ++ List.take n model.restOfFonts
+          , restOfFonts = List.drop n model.restOfFonts
+        }
       , Cmd.none
       )
 
@@ -56,23 +85,31 @@ view model =
     RemoteData.Failure err ->
       [text ("Error: " ++ Debug.toString err)]
     Success allFonts ->
-      let
-          fonts = List.take 8 allFonts -- take gives first n members of the list
-          fontsJoined = List.foldl (\a b -> a ++ "|" ++ b) "" (List.map .family fonts) -- font families joined with | as per API
-      in
-      [Html.node "link" [rel "stylesheet"
-      , href ("https://fonts.googleapis.com/css?family=" ++ fontsJoined) -- TODO put +s between words and URL encode
-      ] []
-      , div [] (List.map (\font ->
-        div [] [
-          div [] [text font.family]
-          , div [style "font-family" ("'" ++ font.family ++ "', " ++ font.category)] [text model.text]
-          , br [] []
-        ]
-
-        )
-        fonts)]
+      [ div [] (List.map link (groupsOf n model.visibleFonts))
+      , fontsView model.visibleFonts model.text
+      , button [ onClick MoreFonts ] [ text "More" ]
+      ]
  }
+
+fontsView fonts text_ =
+  div [] (List.map (fontView text_) fonts)
+
+fontView text_ { family, category } =
+  div [] [
+    div [] [text family]
+    , div [style "font-family" ("'" ++ family ++ "', " ++ category)] [text text_]
+    , br [] []
+  ]
+
+link : Fonts -> Html msg
+link fonts =
+  let
+      fontsJoined = List.foldl (\a b -> a ++ "|" ++ b) "" (List.map .family fonts) -- font families joined with | as per API -- at the moment, | is also added to the end. List.intersperse may be able to help.
+  in
+      Html.node "link"
+        [ rel "stylesheet"
+        , href ("https://fonts.googleapis.com/css?family=" ++ fontsJoined) -- TODO put +s between words and URL encode
+        ] []
 
 
 
